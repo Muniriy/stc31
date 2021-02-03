abf89a68b6980d2bf8fc0d2f90bf8b5542d3f4a8
Mruni <munir.makhmutov@gmail.com>
2021-02-03 15:27:33 +0300

    Homework 19 is updated

--- lesson19/src/main/java/task1/DBUtils.java
+++ lesson19/src/main/java/task1/DBUtils.java
@@ -25,16 +25,34 @@ public class DBUtils {
                     "CREATE TABLE laptops (itemId bigserial primary key," +
                     "title varchar(30) NOT NULL, brand varchar(15) NOT NULL," +
                     "manufacturer varchar(20) NOT NULL, os varchar(10) NOT NULL, " +
-                    "cpu varchar(20) NOT NULL, ram varchar(10) NOT NULL," +
-                    "screenSize integer NOT NULL);" +
+                    "cpu varchar(20) NOT NULL, ram integer NOT NULL," +
+                    "screenSize float NOT NULL);" +
                     "\n" +
                     "CREATE TABLE customers (id bigserial primary key," +
                     "name varchar(20) NOT NULL, birthday date NOT NULL," +
-                    "basket varchar(20), payment_card_id integer);" +
+                    "basket varchar(20), payment_card_id bigint);" +
                     "\n" +
                     "CREATE TABLE payment_cards (id bigserial primary key," +
                     "card_number bigint NOT NULL, issue date NOT NULL," +
-                    "cvc smallint NOT NULL, card_holder varchar(40) NOT NULL)");
+                    "cvc smallint NOT NULL, card_holder varchar(40) NOT NULL);" +
+                    "\n" +
+                    "INSERT INTO customers VALUES (1, 'Peter', '2000-01-01', " +
+                    "'MacBook Air M1 2020', 123);" +
+                    "\n" +
+                    "INSERT INTO customers VALUES (2, 'Tom', '1990-05-08', " +
+                    "'MacBook Pro M1 2020');" +
+                    "\n" +
+                    "INSERT INTO customers VALUES (3, 'Mick', '1995-12-30');" +
+                    "\n" +
+                    "INSERT INTO customers (id, name, birthday, payment_card_id) " +
+                    "VALUES (4, 'Alice', '1997-10-09', 536);" +
+                    "\n" +
+                    "INSERT INTO customers VALUES (5, 'Amanda', '2001-06-06', " +
+                    "'Lenovo Idea', 919);" +
+                    "\n" +
+                    "INSERT INTO customers VALUES " +
+                    "(6, 'Vicky', '2002-03-16', 'Asus Zenbook', 493);"
+            );
         }
     }
 }
--- lesson19/src/main/java/task1/OnlineStoreJdbc.java
+++ lesson19/src/main/java/task1/OnlineStoreJdbc.java
@@ -3,9 +3,7 @@ package task1;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class OnlineStoreJdbc {
 
@@ -20,22 +18,17 @@ public class OnlineStoreJdbc {
      * @param args Array with parameters of the program
      */
     public static void main(String[] args) {
-        try {
-            connectDatabase();
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
+        workWithDatabase();
     }
 
     /**
      * This method creates connection and renews the
-     * database
-     *
-     * @throws ClassNotFoundException exception of not founding
-     *                                a class
-     * @throws SQLException           the SQL exception
+     * database and then makes modifications, insertions
+     * and updates into tables showing work of statements,
+     * prepared statements, batching, manual commits and
+     * savepoints
      */
-    private static void connectDatabase() throws ClassNotFoundException, SQLException {
+    private static void workWithDatabase() {
         String url = "jdbc:postgresql://localhost:5432/online_store";
         String user = "postgres";
         String pass = "postgress";
@@ -44,6 +37,97 @@ public class OnlineStoreJdbc {
             log.info("DB is successfully connected");
             DBUtils.renewDatabase(connection);
             log.info("DB is renewed");
+            makePreparedStatement(connection);
+            makeBatching(connection);
+            makeManualCommits(connection);
+        } catch (SQLException throwables) {
+            log.error(throwables.getMessage());
+        }
+    }
+
+    /**
+     * This method allows to do manual commits to DB
+     * with using savepoint
+     *
+     * @param connection the DB connection object
+     * @throws SQLException the SQL exception
+     */
+    private static void makeManualCommits(Connection connection) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            connection.setAutoCommit(false);
+            statement.executeUpdate("INSERT INTO laptops VALUES (111, " +
+                    "'MacBook Air M1 2020', 'Apple', 'China', 'macOS', 'M1', " +
+                    "8, 13)");
+            log.info("Manual commit is performed executed successfully");
+
+            Savepoint savepoint = connection.setSavepoint();
+            statement.executeUpdate("INSERT INTO laptops VALUES (222, " +
+                    "'MacBook Pro M1 2020', 'Apple', 'China', 'macOS', 'M1', " +
+                    "16, 13)");
+            connection.rollback(savepoint);
+            connection.commit();
+
+        } catch (SQLException throwables) {
+            connection.rollback();
+            log.error(throwables.getMessage());
+        }
+    }
+
+    /**
+     * This method calls a Prepared statement query
+     * and uses batching to add gift payment card to
+     * users without cards
+     *
+     * @param connection the DB connection object
+     * @throws SQLException the SQL exception
+     */
+    private static void makeBatching(Connection connection) throws SQLException {
+        Integer[] idArgs = new Integer[]{1, 2, 3};
+        try (PreparedStatement preparedStatement = connection.prepareStatement(
+                "UPDATE customers SET payment_card_id=999 " +
+                        "WHERE payment_card_id IS NULL " +
+                        "AND id = ?")) {
+            for (Integer id : idArgs) {
+                preparedStatement.setInt(1, id);
+                preparedStatement.addBatch();
+            }
+            preparedStatement.executeBatch();
+            log.info("Batching executed successfully");
+        }
+    }
+
+    /**
+     * This method calls a Prepared statement query
+     * and puts 2 values
+     *
+     * @param connection the DB connection object
+     */
+    private static void makePreparedStatement(Connection connection) throws SQLException {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(
+                "SELECT * FROM customers " +
+                        "WHERE basket IS NOT NULL " +
+                        "AND birthday >= ? " +
+                        "AND name <> ?")) {
+            preparedStatement.setDate(1, Date.valueOf("2000-01-01"));
+            preparedStatement.setString(2, "Amanda");
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                StringBuilder result = new StringBuilder("The result of a query is\n");
+                while (resultSet.next()) {
+                    result.append("id=");
+                    result.append(resultSet.getInt("id"));
+                    result.append("; name=");
+                    result.append(resultSet.getString("name"));
+                    result.append("; birthday=");
+                    result.append(resultSet.getDate("birthday"));
+                    result.append("; basket=");
+                    result.append(resultSet.getString("basket"));
+                    result.append("; payment_card_id=");
+                    result.append(resultSet.getInt("payment_card_id"));
+                    result.append("\n");
+                }
+                log.info(result);
+            }
         }
     }
 
